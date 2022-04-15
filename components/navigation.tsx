7320a3f667735ad25f6bbad4be27508594e97f43
kouya17 <17383696+kouya17@users.noreply.github.com>
2022-04-15 22:04:17 +0900

    ci: add deploy action

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { MenuItem } from "../lib/models";
+import Link from 'next/link'
 
 interface NavigationProps {
   title: string;
@@ -9,9 +10,11 @@ interface NavigationProps {
 const Navigation: FunctionComponent<NavigationProps> = ({ title, menu }) => (
   <nav className="sticky top-0 py-5 bg-gray-700 text-white opacity-80 z-50">
     <div className="flex justify-center items-center">
-      <a href="/" className="px-5 absolute left-0">
-        <div className="self-center text-lg font-semibold whitespace-nowrap">{title}</div>
-      </a>
+      <Link href="/">
+        <a className="px-5 absolute left-0">
+          <div className="self-center text-lg font-semibold whitespace-nowrap">{title}</div>
+        </a>
+      </Link>
     </div>
   </nav>
 );
