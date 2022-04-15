74d00efb878bccb554b55d22afd5bf6d38ce63b4
kouya17 <kouya17@users.noreply.github.com>
2022-04-15 13:10:45 +0000

    deploy: 338c7db098ff9e4dd0cc73b5cf25447af187569e

---
+++ _next/static/chunks/ae51ba48-8cf69ba0deeadb60.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[260],{155:function(t,n,e){e.d(n,{xcL:function(){return i}});var r=e(8357);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M256 112v288m144-144H112"}}]})(t)}}}]);
