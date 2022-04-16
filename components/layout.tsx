f3985064d94cf29c5ab91bd1195ccd91dbcbf59a
kouya17 <17383696+kouya17@users.noreply.github.com>
2022-04-16 21:01:29 +0900

    fix: og image address

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,7 +19,7 @@ export default function Layout({
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary" />
         <meta property="og:description" content="Searchable Shelfのページです。" />
-        <meta property="og:image" content="/logo.png" />
+        <meta property="og:image" content="https://sshelf.work/logo.png" />
       </Head>
       <Navigation
         title={siteTitle}
